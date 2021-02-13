8db52b22216b8e00f999b3e81ff85f5583ad39ca
Vadim1570 <khamzin-vadim@yandex.ru>
2021-02-13 11:33:09 +0300

    Добавил народы на вкладку 1992-2020

--- App/html/script.js
+++ App/html/script.js
@@ -82,7 +82,7 @@ let inner_text = document.createElement('div');
 	tabBlackClick();
 
 	//нажать на кнопку
-	var btn = document.getElementById('surgut_1980_1991');
+	var btn = document.getElementById('surgut_1992_2020');
 	var evt = new Event('click');
 	btn.dispatchEvent(evt); // evt.target = btn;
 	menuButtonClick(evt);
